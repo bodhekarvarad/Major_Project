b1386b2a9e501403a3b8134fe9712fc3afe6968c
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-02-02 10:19:48 +0530

    Adding front-end

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const mongoose=require('mongoose');
 const path=require('path');
 const methodOverride=require("method-override");
 const Listing = require('./models/listing');
+const ejsMate=require('ejs-mate');
+
+
+
 app.listen(port,()=>{console.log(`Server running on port ${port}`);});
 
 app.get('/',(req,res)=>{
@@ -37,6 +41,8 @@ async function main() {
 //   console.log("Sample listing saved successfully");
 //     res.send("Sample listing saved successfully");
 // });
+app.use(express.static(path.join(__dirname,"public")));
+app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
